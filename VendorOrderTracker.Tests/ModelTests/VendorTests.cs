bb1e5faaeea05460348d5a87abdd63aa19498ac5
E Luckie <eluckie.d@gmail.com>
2023-03-03 09:58:31 -0700

    pass second test to get Vendor name

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -11,8 +11,14 @@ namespace VendorOrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("CompanyName");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetName_ReturnsVendorName_String()
+    {
+      Vendor newVendor = new Vendor("CompanyName");
+      Assert.AreEqual("CompanyName", newVendor.Name);
     }
   }
 }
--- VendorOrderTracker/Models/Vendor.cs
+++ VendorOrderTracker/Models/Vendor.cs
@@ -4,6 +4,11 @@ namespace VendorOrderTracker.Models
 {
   public class Vendor
   {
-    
+    public string Name { get; set; }
+
+    public Vendor(string name)
+    {
+      Name = name;
+    }
   }
 }
